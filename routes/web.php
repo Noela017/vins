20f196660d3cead9d0bace4b3449c8d00d7d264a
silaschege <silaskimani4080@gmail.com>
2020-11-07 19:25:58 +0300

    new changes

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/', "PagesController@index");
+
+Route::get('/about', function () {
     return view('welcome');
 });
